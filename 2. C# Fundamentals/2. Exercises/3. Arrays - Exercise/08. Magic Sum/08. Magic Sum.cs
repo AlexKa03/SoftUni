aa3ce8e0262b76be9bd7543ac24ef5e532c5880a
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:50:00 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/3. Arrays - Exercise/08. Magic Sum/08. Magic Sum.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (j > i)
+                    {
+                        if (numbers[i] + numbers[j] == sum)
+                        {
+                            Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
