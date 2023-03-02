03c1b80efe4713e75d28119931170de4815e4516
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:10:38 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/1. Basic Syntax,Conditional Statements and Loops-Lab/07. Theatre Promotion/07. Theatre Promotion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+
+            int price = 0;
+
+            while (age >= 0 && age <= 122)
+            {
+                if (dayOfWeek == "weekday")
+                {
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 12;
+                    }
+                    else if (age >= 19 && age <= 64)
+                    {
+                        price = 18;
+                    }
+                    else if (age >= 65 && age <= 122)
+                    {
+                        price = 12;
+                    }
+                }
+                else if (dayOfWeek == "weekend")
+                {
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 15;
+                    }
+                    else if (age >= 19 && age <= 64)
+                    {
+                        price = 20;
+                    }
+                    else if (age >= 65 && age <= 122)
+                    {
+                        price = 15;
+                    }
+                }
+                else
+                {
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 5;
+                    }
+                    else if (age >= 19 && age <= 64)
+                    {
+                        price = 12;
+                    }
+                    else if (age >= 65 && age <= 122)
+                    {
+                        price = 10;
+                    }
+                }
+
+                Console.WriteLine($"{price}$");
+                break;
+            }
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
