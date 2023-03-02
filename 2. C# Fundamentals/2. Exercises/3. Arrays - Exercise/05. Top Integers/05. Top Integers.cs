d6fed35fe42c9bd467a8ba43044bf2a23d3863ab
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:47:07 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/3. Arrays - Exercise/05. Top Integers/05. Top Integers.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == array.Length - 1)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                else if (array[i] > array[i + 1])
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+        }
+    }
+}
