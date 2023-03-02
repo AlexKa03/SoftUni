4a5b6798fac5614a88109a509a6cc4a8dfe17b2c
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:22:23 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/4. Methods - Lab/11. Math operations/11. Math operations.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOne = int.Parse(Console.ReadLine());
+        string symbol = Console.ReadLine();
+        int numberTwo = int.Parse(Console.ReadLine());
+
+        double result = Calculate(numberOne, numberTwo, symbol);
+        Console.WriteLine(result);
+    }
+
+    static double Calculate(int  a, int b, string symbol)
+    {
+        double result = 0;
+
+        switch (symbol)
+        {
+            case "*":
+                result = a * b;
+                break;
+            case "+":
+                result = a + b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+        }
+
+        return result;
+    }
+}
