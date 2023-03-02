6a35bdd02c254a92c71687a46280fb0194c56af9
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:20:53 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/2. Data Types and Variables - Lab/04. Centuries to Minutes/04. Centuries to Minutes.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        byte centuries = byte.Parse(Console.ReadLine());
+
+        ushort years = (ushort)(centuries * 100);
+        double days = Math.Floor(years * 365.2422);
+        uint hours = (uint)(days * 24);
+        ulong minutes = hours * 60;
+
+        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+    }
+}
