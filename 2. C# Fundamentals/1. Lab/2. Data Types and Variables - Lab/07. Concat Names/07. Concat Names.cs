695b7655983843670c7e5ec8fa1d178dfd283769
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:23:53 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/2. Data Types and Variables - Lab/07. Concat Names/07. Concat Names.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string delimiter = Console.ReadLine();
+
+        Console.WriteLine($"{firstName}{delimiter}{lastName}");
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/2. Data Types and Variables - Lab/09. Chars to String/09. Chars to String.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        char lineOne = char.Parse(Console.ReadLine());
+        char lineTwo = char.Parse(Console.ReadLine());
+        char lineThree = char.Parse(Console.ReadLine());
+
+        Console.WriteLine(lineOne.ToString() + lineTwo.ToString() + lineThree.ToString());
+    }
+}
