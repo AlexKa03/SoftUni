5724d57176f23cd44d0850d1202c5a842d2346e6
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:26:57 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/01. Ages/01. Ages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            string ageGroup = "";
+
+            if (age >= 0 && age <= 2)
+            {
+                ageGroup = "baby";
+            }
+            else if (age >= 3 && age <= 13)
+            {
+                ageGroup = "child";
+            }
+            else if (age >= 14 && age <= 19)
+            {
+                ageGroup = "teenager";
+            }
+            else if (age >= 20 && age <= 65)
+            {
+                ageGroup = "adult";
+            }
+            else if (age >= 66)
+            {
+                ageGroup = "elder";
+            }
+
+            Console.WriteLine(ageGroup);
+        }
+    }
+}
---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople = int.Parse(Console.ReadLine());
+            string typeGroup = Console.ReadLine();
+            string dayWeek = Console.ReadLine();
+
+            double total = 0.00;
+
+            if (dayWeek == "Friday")
+            {
+                if (typeGroup == "Students")
+                {
+                    total = 8.45 * countPeople;
+
+                    if (countPeople >= 30)
+                    {
+                        total *= 0.85;
+                    }
+                }
+                else if (typeGroup == "Business")
+                {
+                    total = 10.90 * countPeople;
+
+                    if (countPeople >= 100)
+                    {
+                        total = 10.90 * (countPeople - 10);
+                    }
+                }
+                else if (typeGroup == "Regular")
+                {
+                    total = 15 * countPeople;
+
+                    if (countPeople >= 10 && countPeople <= 20)
+                    {
+                        total *= 0.95;
+                    }
+                }
+            }
+            else if (dayWeek == "Saturday")
+            {
+                if (typeGroup == "Students")
+                {
+                    total = 9.80 * countPeople;
+
+                    if (countPeople >= 30)
+                    {
+                        total *= 0.85;
+                    }
+                }
+                else if (typeGroup == "Business")
+                {
+                    total = 15.60 * countPeople;
+
+                    if (countPeople >= 100)
+                    {
+                        total = 15.60 * (countPeople - 10);
+                    }
+                }
+                else if (typeGroup == "Regular")
+                {
+                    total = 20 * countPeople;
+
+                    if (countPeople >= 10 && countPeople <= 20)
+                    {
+                        total *= 0.95;
+                    }
+                }
+            }
+            else if (dayWeek == "Sunday")
+            {
+                if (typeGroup == "Students")
+                {
+                    total = 10.46 * countPeople;
+
+                    if (countPeople >= 30)
+                    {
+                        total *= 0.85;
+                    }
+                }
+                else if (typeGroup == "Business")
+                {
+                    total = 16 * countPeople;
+
+                    if (countPeople >= 100)
+                    {
+                        total = 16 * (countPeople - 10);
+                    }
+                }
+                else if (typeGroup == "Regular")
+                {
+                    total = 22.50 * countPeople;
+
+                    if (countPeople >= 10 && countPeople <= 20)
+                    {
+                        total *= 0.95;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total price: {total:f2}");
+        }
+    }
+}
