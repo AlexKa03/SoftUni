8e5741988979292a7759bce2ac19c7c7be68a22b
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:42:04 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/2. Data Types and Variables - Exercise/10. Poke Mon/10. Poke Mon.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            uint power = uint.Parse(Console.ReadLine());
+            uint distance = uint.Parse(Console.ReadLine());
+            byte exhaustion = byte.Parse(Console.ReadLine());
+
+            uint originalPower = power;
+            ushort targets = 0;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                targets++;
+
+                if ((originalPower * 0.5 == power) && exhaustion != 0)
+                {
+                    power = power / exhaustion;
+                }
+            }
+
+            Console.WriteLine(power);
+            Console.WriteLine(targets);
+        }
+    }
+}
---
+++ 2. C# Fundamentals/2. Exercises/2. Data Types and Variables - Exercise/11. Snowballs/11. Snowballs.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        {
+            byte snowballs = byte.Parse(Console.ReadLine());
+
+            BigInteger bestValue = 0;
+
+            ushort beestSnow = 0;
+            ushort bestTime = 0;
+            ushort bestQuality = 0;
+
+            for (int i = 1; i <= snowballs; i++)
+            {
+                ushort snowballSnow = ushort.Parse(Console.ReadLine());
+                ushort snowballTime = ushort.Parse(Console.ReadLine());
+                ushort snowballQuality = ushort.Parse(Console.ReadLine());
+                /*
+                   Well done SoftUni, never presented how BigInteger
+                   works and didn't say in the exercise description
+                   that we should be looking for something like that :)
+                */
+                BigInteger value = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (value >= bestValue)
+                {
+                    bestValue = value;
+                    beestSnow = snowballSnow;
+                    bestTime = snowballTime;
+                    bestQuality = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($"{beestSnow} : {bestTime} = {bestValue} ({bestQuality})");
+        }
+    }
+}
