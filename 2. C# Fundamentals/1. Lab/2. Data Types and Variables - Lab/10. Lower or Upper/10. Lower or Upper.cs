b81b984b0ffd0cf2aa206a457751535550ca72ed
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:26:41 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/2. Data Types and Variables - Lab/10. Lower or Upper/10. Lower or Upper.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        char givenChar = char.Parse(Console.ReadLine());
+
+        bool isUpper = true;
+
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (letter == givenChar)
+            {
+                Console.WriteLine("lower-case");
+                isUpper = false;
+                break;
+            }
+        }
+
+        if (isUpper)
+        {
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                if (letter == givenChar)
+                {
+                    Console.WriteLine("upper-case");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/2. Data Types and Variables - Lab/12. Refactor Special Numbers/12. Refactor Special Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int number = 1; number <= n; number++)
+        {
+            int sumOfDigits = 0;
+            int digits = number;
+
+            while (digits > 0)
+            {
+                sumOfDigits += digits % 10;
+                digits = digits / 10;
+            }
+
+            bool isSpecial = (sumOfDigits == 5) ||
+                       (sumOfDigits == 7) ||
+                       (sumOfDigits == 11);
+
+            Console.WriteLine($"{number} -> {isSpecial}");
+        }
+    }
+}
