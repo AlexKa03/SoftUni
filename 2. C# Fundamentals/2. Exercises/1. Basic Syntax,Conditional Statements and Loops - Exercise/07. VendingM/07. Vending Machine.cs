ec0713de81f77c46d477288a86919b5c0b3bc579
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:30:16 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/07. VendingM/07. Vending Machine.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _07._VendingM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            double total = 0;
+
+            while (command != "Start")
+            {
+                double money = double.Parse(command);
+
+                switch (money)
+                {
+                    case 0.1:
+                        total += 0.1;
+                        break;
+                    case 0.2:
+                        total += 0.2;
+                        break;
+                    case 0.5:
+                        total += 0.5;
+                        break;
+                    case 1:
+                        total += 1;
+                        break;
+                    case 2:
+                        total += 2;
+                        break;
+                    default:
+                        Console.WriteLine($"Cannot accept {money}");
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                switch (command)
+                {
+                    case "Nuts":
+                        if (total >= 2)
+                        {
+                            Console.WriteLine($"Purchased {command.ToLower()}");
+                            total -= 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Water":
+                        if (total >= 0.7)
+                        {
+                            Console.WriteLine($"Purchased {command.ToLower()}");
+                            total -= 0.7;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Crisps":
+                        if (total >= 1.5)
+                        {
+                            Console.WriteLine($"Purchased {command.ToLower()}");
+                            total -= 1.5;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Soda":
+                        if (total >= 0.8)
+                        {
+                            Console.WriteLine($"Purchased {command.ToLower()}");
+                            total -= 0.8;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Coke":
+                        if (total >= 1)
+                        {
+                            Console.WriteLine($"Purchased {command.ToLower()}");
+                            total -= 1;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {total:f2}");
+        }
+    }
+}
---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/09. PadawanE/09. Padawan Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09._PadawanE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            double studentsCount = double.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double lightsabers = studentsCount + Math.Ceiling(studentsCount * 0.1);
+            double robes = studentsCount;
+            double belts = studentsCount - Math.Floor(studentsCount / 6);
+
+            double total = (lightsabers * lightsaberPrice) + (robes * robesPrice) + (belts * beltsPrice);
+
+            if (money >= total)
+            {
+                Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {total - money:f2}lv more.");
+            }
+        }
+    }
+}
