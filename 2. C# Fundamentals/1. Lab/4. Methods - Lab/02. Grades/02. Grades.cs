fe1548f82d9b37e5edf1744ae8f0e9e650dff944
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:11:18 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/4. Methods - Lab/02. Grades/02. Grades.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        string gradeLetters = string.Empty;
+
+        CheckGrades(grade, gradeLetters);
+    }
+
+    static void CheckGrades(double grade, string gradeLetters)
+    {
+        if (grade >= 2 && grade < 3)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (grade >= 3 && grade < 3.50)
+        {
+            Console.WriteLine("Poor");
+        }
+        else if (grade >= 3.50 && grade < 4.50)
+        {
+            Console.WriteLine("Good");
+        }
+        else if (grade >= 4.50 && grade < 5.50)
+        {
+            Console.WriteLine("Very good");
+        }
+        else if (grade >= 5.50 && grade <= 6)
+        {
+            Console.WriteLine("Excellent");
+        }
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/4. Methods - Lab/03. Calculations/03. Calculations.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string action = Console.ReadLine();
+        int numOne = int.Parse(Console.ReadLine());
+        int numTwo = int.Parse(Console.ReadLine());
+
+        switch (action) 
+        {
+            case "add":
+                Add(numOne, numTwo);
+                break;
+            case "multiply":
+                Multiply(numOne, numTwo);
+                break;
+            case "subtract":
+                Subtract(numOne, numTwo);
+                break;
+            case "divide":
+                Divide(numOne, numTwo);
+                break;
+        }
+    }
+
+    static void Add(int numOne, int numTwo)
+    {
+        Console.WriteLine(numOne + numTwo);
+    }
+    static void Multiply(int numOne, int numTwo)
+    {
+        Console.WriteLine(numOne * numTwo);
+    }
+    static void Subtract(int numOne, int numTwo)
+    {
+        Console.WriteLine(numOne - numTwo);
+    }
+    static void Divide(int numOne, int numTwo)
+    {
+        Console.WriteLine(numOne / numTwo);
+    }
+}
