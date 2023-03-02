2721ef2baa2fe70ae338f65da3f8dfcd4d0a9a01
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:31:30 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/10. Rage/10. Rage Expenses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _10._Rage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double monitorPrice = double.Parse(Console.ReadLine());
+
+            double expenses = 0;
+            int count = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    expenses += headsetPrice;
+                }
+                if (i % 3 == 0)
+                {
+                    expenses += mousePrice;
+                }
+                if (i % 6 == 0)
+                {
+                    count++;
+                    expenses += keyboardPrice;
+
+                    if (count % 2 == 0)
+                    {
+                        expenses += monitorPrice;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/11. Orders/11. Orders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+
+            double total = 0.00;
+
+            for (int i = 1; i <= orders; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsuleCount = int.Parse(Console.ReadLine());
+
+                double orderTotal = capsulePrice * (days * capsuleCount);
+                total += orderTotal;
+
+                Console.WriteLine($"The price for the coffee is: ${orderTotal:f2}");
+            }
+
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
