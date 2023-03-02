3ba7a96b406015316f5e591fe070ebbbc611efa2
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:56:31 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/3. Arrays - Lab/07. Equal Arrays/07. Equal Arrays.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            int[] arrayOne = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] arrayTwo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isEqual = false;
+            int sum = 0;
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if (arrayOne[i] != arrayTwo[i])
+                {
+                    isEqual = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    sum += arrayOne[i];
+                    isEqual = true;
+                }
+            }
+
+            if (isEqual)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/3. Arrays - Lab/08. Condense Array to Number/08. Condense Array to Number.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            int condensed = array.Length - 1;
+
+            if (array.Length == 1)
+            {
+                Console.WriteLine(array[0]);
+                return;
+            }
+
+            for (int i = 0; i < condensed; i++)
+            {
+                int[] modifiedArray = new int[array.Length - 1];
+
+                for (int j = 0; j < modifiedArray.Length; j++)
+                {
+                    modifiedArray[j] = array[j] + array[j + 1];
+                }
+
+                array = modifiedArray;
+                sum = modifiedArray[0];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
