c46407de4cd32085a99faec7e67abca79aa63d62
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 20:41:24 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/3. Arrays - Lab/02. Print Numbers in Reverse Order/02. Print Numbers in Reverse Order.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i <= n - 1; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                Console.Write(numbers[(numbers.Length - 1) - i] + " ");
+            }
+        }
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/3. Arrays - Lab/03. Rounding Numbers/03. Rounding Numbers.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                if (numbers[i] == 0 || numbers[i] == -0)
+                {
+                    numbers[i] = 0;
+                }
+                else
+                {
+                    numbers[i] = numbers[i];
+                }
+            }
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
