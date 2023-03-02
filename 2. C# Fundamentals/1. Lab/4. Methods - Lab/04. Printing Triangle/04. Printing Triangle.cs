320613e652814fa4d317c5574565e24e63cbb57e
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:19:26 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/4. Methods - Lab/04. Printing Triangle/04. Printing Triangle.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        PrintTriangle(number);
+    }
+
+    static void PrintTriangle(int number)
+    {
+        for (int lineUp = 1; lineUp <= number; lineUp++)
+        {
+            PrintLine(1, lineUp);
+        }
+        for (int lineDown = number - 1; lineDown >= 1; lineDown--)
+        {
+            PrintLine(1, lineDown);
+        }
+    }
+
+    static void PrintLine (int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write("{0} ", i);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/4. Methods - Lab/05. Orders/05. Orders.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string product = Console.ReadLine();
+        int quantity = int.Parse(Console.ReadLine());
+
+        OrderCalculation(product, quantity);
+    }
+
+    static void OrderCalculation(string product, int quantity)
+    {
+        double order = 0;
+
+        switch (product)
+        {
+            case "coffee":
+                order = 1.50 * quantity;
+                break;
+            case "water":
+                order = 1 * quantity;
+                break;
+            case "coke":
+                order = 1.40 * quantity;
+                break;
+            case "snacks":
+                order = 2 * quantity;
+                break;
+        }
+
+        Console.WriteLine($"{order:f2}");
+    }
+}
