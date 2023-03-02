40744334ff96b82f3dfe0dfa769b682066524780
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 19:58:43 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/1. Lab/1. Basic Syntax,Conditional Statements and Loops-Lab/04. Back In 30 Minutes/04. Back In 30 Minutes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            if (minutes < 30)
+            {
+                minutes += 30;
+            }
+            else
+            {
+                minutes -= 30;
+
+                if (hours < 23)
+                {
+                    hours++;
+                }
+                else
+                {
+                    hours = 0;
+                }
+            }
+
+            if (minutes <= 9)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+        }
+    }
+}
---
+++ 2. C# Fundamentals/1. Lab/1. Basic Syntax,Conditional Statements and Loops-Lab/05. Month Printer/05. Month Printer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05._Month_Printer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthNumber = int.Parse(Console.ReadLine());
+            string monthLetters;
+
+            switch (monthNumber) 
+            {
+                case 1:
+                    monthLetters = "January";
+                    break;
+                case 2:
+                    monthLetters = "February";
+                    break;
+                case 3:
+                    monthLetters = "March";
+                    break;
+                case 4:
+                    monthLetters = "April";
+                    break;
+                case 5:
+                    monthLetters = "May";
+                    break;
+                case 6:
+                    monthLetters = "June";
+                    break;
+                case 7:
+                    monthLetters = "July";
+                    break;
+                case 8:
+                    monthLetters = "August";
+                    break;
+                case 9:
+                    monthLetters = "September";
+                    break;
+                case 10:
+                    monthLetters = "October";
+                    break;
+                case 11:
+                    monthLetters = "November";
+                    break;
+                case 12:
+                    monthLetters = "December";
+                    break;
+                default:
+                    monthLetters = "Error!";
+                    break;
+            }
+
+            Console.WriteLine(monthLetters);
+        }
+    }
+}
