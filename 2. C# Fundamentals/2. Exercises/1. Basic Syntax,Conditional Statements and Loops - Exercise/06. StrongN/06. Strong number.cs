fa648c676fabf263acfa08e9c166f13194483ebe
Alex Kadiyski <65862735+AlexKa03@users.noreply.github.com>
2023-03-02 21:28:41 +0200

    Uploaded files

---
+++ 2. C# Fundamentals/2. Exercises/1. Basic Syntax,Conditional Statements and Loops - Exercise/06. StrongN/06. Strong number.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._StrongN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int check = number;
+
+            int total = 0;
+
+            while (number > 0)
+            {
+                int n = number % 10;
+                int factorial = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    factorial *= i;
+                }
+                total += factorial;
+                number = number / 10;
+            }
+
+            if (total == check)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
