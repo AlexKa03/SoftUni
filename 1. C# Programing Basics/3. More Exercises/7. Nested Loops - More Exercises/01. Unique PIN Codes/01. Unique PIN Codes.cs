4ee5b732df7ddec9a02bf878087a2b73ec5f6912
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2024-02-12 16:57:39 +0200

    Update
    Update

---
+++ 1. C# Programing Basics/3. More Exercises/7. Nested Loops - More Exercises/01. Unique PIN Codes/01. Unique PIN Codes.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int firstMax = int.Parse(Console.ReadLine());
+        int secondMax = int.Parse(Console.ReadLine());
+        int thirdMax = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= firstMax; i += 2)
+        {
+            for (int j = 2; j <= secondMax; j++)
+            {
+                if (j == 2 || j == 3 || j == 5 || j == 7)
+                {
+                    for (int r = 2; r <= thirdMax; r += 2)
+                    {
+                        Console.WriteLine($"{i} {j} {r}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1. C# Programing Basics/3. More Exercises/7. Nested Loops - More Exercises/02. Letters Combinations/02. Letters Combinations.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        char firstLetter = char.Parse(Console.ReadLine());
+        char lastLetter = char.Parse(Console.ReadLine());
+        char skipLetter = char.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (char i = firstLetter; i <= lastLetter; i++)
+        {
+            if (i != skipLetter)
+            {
+                for (char j = firstLetter; j <= lastLetter; j++)
+                {
+                    if (j != skipLetter)
+                    {
+                        for (char k = firstLetter; k <= lastLetter; k++)
+                        {
+                            if (k != skipLetter)
+                            {
+                                Console.Write($"{i}{j}{k} ");
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.Write(count);
+    }
+}
